ec0c28165f24c0a7bd5bcf66cc90b31d6884fc92
RelishedChicken <veerajvadgama@gmail.com>
2024-09-08 00:45:30 +0100

    two word context

--- index.js
+++ index.js
@@ -16,65 +16,128 @@ function ingestText(){
     word_array = inputText.split(" ");
 
     //Take that word array and find common following words for each word
-    for(var i=0;i<word_array.length - 1;i++){
+    for(var i=0;i<word_array.length - 2;i++){
 
         var word_window = [];
 
         //Store this and the next word (after cleaning them)
-        word_window[0] = removePunctuation(word_array[i]).toLowerCase();
-        word_window[1] = removePunctuation(word_array[i+1]).toLowerCase();
+        word_window[0] = removePunctuation(word_array[i]);
+        word_window[1] = removePunctuation(word_array[i+1]);
+        word_window[2] = removePunctuation(word_array[i+2]);
 
-        if(word_window[1] != ""){
-            if(word_lookup[word_window[0]] !== undefined){
-                word_lookup[word_window[0]].push(word_window[1])
-            }else{
-                word_lookup[word_window[0]] = [word_window[1]];
+        if(word_window[0] !== "" && word_window[1] !== "" && word_window[2] !== ""){
+
+            if(!Array.isArray(word_lookup[word_window[0]])){
+                word_lookup[word_window[0]] = new Array();
             }
-        }
-    }    
 
-    console.log(word_lookup);
+            if(!Array.isArray(word_lookup[word_window[0]][word_window[1]])){
+                word_lookup[word_window[0]][word_window[1]] = new Array();
+            }
+
+            if(Array.isArray(word_lookup[word_window[0]]) && Array.isArray(word_lookup[word_window[0]][word_window[1]])){            
+                word_lookup[word_window[0]][word_window[1]].push(word_window[2]);
+            }            
+
+        }    
+
+    }
 
 }
 
 function generateText(){
 
-    //Ingest params
+    //Ingest params & get a random second word
     var startingWord = $('#startingWord').val();
+    var  possibleFollowingWords = Object.keys(word_lookup[startingWord]);
+    var randomSecondWord = possibleFollowingWords[Math.floor(Math.random()*possibleFollowingWords.length)];
     generation_length = $('#generationLength').val();
 
     //Check the word is available
-    if(word_lookup[startingWord] == undefined){
-        alert("This word is NOT in the training data.")
-    }else{
-        //Generate the sentence
-        var sentence = startingWord;
-        var prevWord = startingWord;
-        $('#output').text(sentence);
-        for(i=0;i<generation_length;i++){
+    var word_available = getNextWord(startingWord, randomSecondWord) !== undefined;
 
+    if(word_available){
+
+        //Variable setup
+        var sentence = startingWord + " " + randomSecondWord;
+        var prevWords = [startingWord, randomSecondWord];
+
+
+        //Update the UI output
+        $('#output').text(sentence);
+
+        //Loop through and construct the sentence        
+        for(i=0;i<generation_length;i++){
+            
             //Get the possible next words
-            availableWordSelection = getNextWord(prevWord);
+            availableWordSelection = getNextWord(prevWords[0], prevWords[1]);
+
+            console.log("Next word selection:");
+            if(availableWordSelection == undefined){ //We couldnt find a following word
+                availableWordSelection = getNextRandom(prevWords[1]);
+                if(availableWordSelection == undefined){//We still couldn't find a next word
+                    availableWordSelection = getTotalRandom(prevWords[0], prevWords[1]);
+                }
+            }
+            console.log(availableWordSelection);
+
 
             //Choose one
             var thisWord = availableWordSelection[Math.floor(Math.random()*availableWordSelection.length)];
 
+            //Add to the sentence
             sentence += " " + thisWord;
+            //Update the UI output
             $('#output').text(sentence);
-            prevWord = thisWord;
-        }
-    }
 
-    
+            //Update the list of previous words to be the last and the new word            
+            prevWords = [prevWords[1], thisWord];
+
+        }
+
+    }else{
+        alert("This word is not available in the training data.");
+    }   
 
 }
 
-function getNextWord(text){
-    return word_lookup[text];
+function getNextWord(word1, word2){
+    return word_lookup[word1][word2];
+}
+
+function getNextRandom(word1){    
+    var possibleWord2 = Object.keys(word_lookup[word1]);
+    var word2 = possibleWord2[Math.floor(Math.random()*possibleWord2.length)];
+    return word_lookup[word1][word2];
+}
+
+function getTotalRandom(oldWord1, oldWord2){    
+
+    var possibleWord1 = null;
+    var word1 = null;
+    var possibleWord2 = null;
+    var word2 = null;
+
+    while(notMatched){
+        possibleWord1 = Object.keys(word_lookup);
+        word1 = possibleWord1[Math.floor(Math.random()*possibleWord1.length)];
+        possibleWord2 = Object.keys(word_lookup[word1]);
+        word2 = possibleWord2[Math.floor(Math.random()*possibleWord2.length)];
+        notMatched = (word1 == oldWord1 || word1 == oldWord2) || (word2 == oldWord1 || word2 == oldWord2);
+    }
+
+    possibleOutput = word_lookup[word1][word2];
+    return possibleOutput[Math.floor(Math.random()*possibleOutput.length)];
 }
 
 function removePunctuation(text) {
-    var newText = text.replace(/[\[\].,"\/#!?$%\^&\*;:{}=\-_`~()]/g,"");
-    var newnewText = newText.replace(/[0-9]/g,"");
-    return newnewText.replace(" ","");
+    var skip = true;
+
+    if(!skip){
+        var newText = text.replace(/[\[\].,’"\/#!?$%\^&\*;:{}=\-_`~()]/g,"");
+        var newnewText = newText.replace(/[0-9]/g,"");
+        return newnewText.replace(" ","").toLowerCase();
+    }else{
+        return text;
+    }
 }
